a44b7a5bf9f396c80ba4890f2da7f91a214236f6
Edgar Mila <edgarmila_10@outlook.com>
2024-06-19 14:20:08 +0200

    añadido laravel

--- src/app/dashboard/dashboard/last-bills/last-bills.component.ts
+++ src/app/dashboard/dashboard/last-bills/last-bills.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-last-bills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './last-bills.component.html',
-  styleUrl: './last-bills.component.css'
+  styleUrls: ['./last-bills.component.css'],
+  providers: [DatePipe]
 })
 export class LastBillsComponent {
-  bills=[{name: 'Tomates', price: 1.68}, {name: 'Limones', price: 2.58}]
+
+  bills: {name: string, price: number, date: string|null}[];
+
+  constructor(private datePipe: DatePipe){
+    const currentDate = new Date();
+    const formattedDate = this.datePipe.transform(currentDate, 'dd/MM/yyyy');
+    this.bills = [
+      { name: 'Tomates', price: 1.68, date: formattedDate },
+      { name: 'Limones', price: 2.58, date: formattedDate }
+    ];
+  }
 }
