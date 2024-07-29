dd722e9f093b4865e09b5840ecd206d28ce4ddbb
Edgar Mila <edgarmila_10@outlook.com>
2024-07-29 20:18:35 +0200

    Añadido datatables

--- src/app/dashboard/dashboard/last-bills/last-bills.component.ts
+++ src/app/dashboard/dashboard/last-bills/last-bills.component.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { TicketingService } from '../../../services/ticketing/ticketing.service';
+import { DataTablesModule } from 'angular-datatables';
+import { Config } from 'datatables.net';
+import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-last-bills',
   standalone: true,
-  imports: [CommonModule],
+  imports: [DataTablesModule],
   templateUrl: './last-bills.component.html',
   styleUrls: ['./last-bills.component.css'],
   providers: [DatePipe]
 })
-export class LastBillsComponent {
+export class LastBillsComponent implements OnInit {
 
   bills: string[] = [];
+  dtOptions: Config={}
+  dtTrigger: Subject<any> = new Subject<any>()
 
   constructor(private datePipe: DatePipe, private ticketingService: TicketingService){
-    // const currentDate = new Date();
-    // const formattedDate = this.datePipe.transform(currentDate, 'dd/MM/yyyy');
+  }
+
+  ngOnInit(): void {
+    this.loadBills()
+    this.dtOptions={
+      pagingType: 'full_numbers',
+      pageLength: 25
+    }
+    
+  }
+
+  loadBills(){
     this.ticketingService.gastos.subscribe(res=>{
       this.bills=res
+      console.log('aaaaaaaaaaa')
       console.log(this.bills)
+      this.dtTrigger.next(this.bills);
     })
   }
 }
